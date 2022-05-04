cbbfbd24c35c2235313eba129e1f2e9ad885c247
Dexter-Osha <76421290+Dexter-Osha@users.noreply.github.com>
2022-05-04 14:31:21 -0400

    Sebastian's Brain

---
+++ main.py
@@ -0,0 +1,34 @@
+# IMPORT DISCORD.PY. ALLOWS ACCESS TO DISCORD'S API.
+import discord
+import random
+
+# IMPORT THE OS MODULE.
+import os
+
+# IMPORT LOAD_DOTENV FUNCTION FROM DOTENV MODULE.
+from dotenv import load_dotenv
+
+from discord.ext import commands
+import aiohttp
+
+images = os.path.join(os.getcwd(), "Pigs")
+client = commands.Bot(command_prefix="!")
+
+# GRAB THE API TOKEN FROM THE .ENV FILE.
+DISCORD_TOKEN = 'OTcxNDAwNjQ5MDE3MjgyNjIx.YnJ9Wg.L6-No2kKb4IUcmOK9Zr5mMM_f4M'
+
+def select_random_image_path():
+    return os.path.join(images, random.choice(os.listdir(images)))
+
+@client.event
+async def on_ready():
+	print("Ready")
+
+
+@client.command()
+async def oink(ctx):
+	await ctx.send('Oink', file=discord.File(select_random_image_path()))
+
+
+# EXECUTES THE BOT WITH THE SPECIFIED TOKEN. TOKEN HAS BEEN REMOVED AND USED JUST AS AN EXAMPLE.
+client.run(DISCORD_TOKEN)
